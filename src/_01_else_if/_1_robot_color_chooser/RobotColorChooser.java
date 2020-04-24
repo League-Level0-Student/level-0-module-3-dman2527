88875540cec432f5665a8d64c2fa32f212e4c36c
root <root@cb9e8e55a2a3>
2020-04-24 01:24:04 +0000

    All done with these

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -10,16 +10,64 @@ import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-
-		//1. Create a new Robot
 		Robot Luca = new Robot();
+		Luca.setPenWidth(10);
+		for (int i = 0; i < 3; i++) {
+			String color = JOptionPane.showInputDialog("What color pen would you like me to draw with");		
+			
+	if (color .equals( "red")) {
+				Luca.setPenColor(Color.red);
+			}
+			if (color .equals("blue")) {
+				Luca.setPenColor(Color.blue);
+			}
+			if (color .equals( "green")) {
+				Luca.setPenColor(Color.green);
+			}
+			if (color .equals( "orange")) {
+				Luca.setPenColor(Color.orange);
+			}
+			if (color .equals( "purple")) {
+				Luca.setPenColor(Color.pink);
+			}
+			if (color .equals( "white")) {
+				Luca.setPenColor(Color.white);
+			}
+			if (color .equals( "black")) {
+				Luca.setPenColor(Color.black);
+			}
+			
+	        //6. If the user doesn’t enter anything, choose a random color
+			if (color .equals( "")) {
+				Luca.setRandomPenColor();
+			}
+			
+			Luca.setSpeed(1000);
+			Luca.penDown();
+			Luca.moveTo(300,300);
+			Luca.setAngle(0);
+			Luca.move(100);
+			Luca.turn(270);
+			Luca.move(100);
+			Luca.turn(270);
+			Luca.move(100);
+			Luca.turn(270);
+			Luca.move(100);
+			Luca.turn(270);
+			Luca.move(100); 
+			}
+	}
+		//1. Create a new Robot
+		
 		
 	    //2. Make the robot draw a shape (this will take more than one line of code)		
 		
 		//3. Set the pen width to 10
+		
 		//4. Ask the user what color pen they would like the robot to draw with
+
 		//5. Use an if/else statement to set the pen color that the user requested
-        //6. If the user doesn’t enter anything, choose a random color
+	
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 
@@ -27,5 +75,5 @@ public class RobotColorChooser {
 
 
 
+	
 	}
-}
--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -3,30 +3,52 @@ package _01_else_if._2_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user how many cats they have
+		String Cats = JOptionPane.showInputDialog("How many cats do you have?");
 		// 2. Convert their answer into an int
+		int Cats1 = Integer.parseInt(Cats);
+		
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+		if (Cats1 >= 3) {
+			JOptionPane.showMessageDialog(null,"You are a crazy lady!");
+		}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
+		if (Cats1 < 3 && Cats1 > 0) {
+			playVideo("https://www.youtube.com/watch?v=XyNlqQId-nk");
+			
+		}
+		
 		//    Hint: You can use the && operator 
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
-		
+		if (Cats1 == 0) {
+			playVideo("https://www.youtube.com/watch?v=QnkUljiLDTQ");
+		}
 		
 		
 		
 		
 	}
 
-	static void playVideo(String videoURL) {
-		try {
-			URI uri = new URI(videoURL);
-			java.awt.Desktop.getDesktop().browse(uri);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public static void playVideo(String videoID) {
+		// Workaround for Linux because "Desktop.getDesktop().browse()" doesn't work on
+		// some Linux implementations
+        try{
+		if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+				if (Runtime.getRuntime().exec(new String[] { "which", "xdg-open" }).getInputStream().read() != -1) {
+					Runtime.getRuntime().exec(new String[] { "xdg-open", videoID });
+				}
+		} else {
+				URI uri = new URI(videoID);
+				java.awt.Desktop.getDesktop().browse(uri);
+		}	
+            } catch (Exception e) {
+				e.printStackTrace();
+			}
 	}
 
 }
